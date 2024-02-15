53204a803b8a5ef3ddd97ac316450750833c6573
CODESPACE THOR <144303807+seong-jin-choi@users.noreply.github.com>
2024-02-15 14:29:54 +0900

    푸시알림 추가

--- App.tsx
+++ App.tsx
@@ -1,22 +1,57 @@
 import React from 'react';
-import {SafeAreaView, StatusBar, useColorScheme, View} from 'react-native';
+import {Button, SafeAreaView, StatusBar, StyleSheet} from 'react-native';
+import * as Notifications from 'expo-notifications';
 
-import {Colors} from 'react-native/Libraries/NewAppScreen';
+Notifications.setNotificationHandler({
+  handleNotification: async () => {
+    return {
+      shouldPlaySound: false, // 알림소리를 설정할 것인가? (ex 카톡은 알림을 받을때 '까똑' 소리가 남)
+      shouldSetBadge: false, // 보통 앱 아이콘 오른쪽 위에 있는 '받은 알림 갯수' 뱃지를 부착할 것인가?
+      shouldShowAlert: true, // 상단바에 알림을 보여줄 것인가?
+    };
+  },
+});
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+  async function handleScheduleAlarm() {
+    // 알림 권한요청
+    await Notifications.requestPermissionsAsync({
+      ios: {
+        allowAlert: true,
+        allowBadge: true,
+        allowSound: true,
+        allowAnnouncements: true,
+      },
+    });
+    await Notifications.scheduleNotificationAsync({
+      content: {
+        title: '똑똑, 할일을 완료하셨나요?',
+        body: 'Getch가 추천하는 오늘의 할일을 완료하셨나요?',
 
+        // data Property는 키-값을 전송할 수 있음.
+        // 가령 특정 할일을 식별할 id를 첨부해야 할 때 유용함
+        data: {todoID: 'ID_KEY'},
+      },
+      trigger: {seconds: 3},
+    });
+  }
   const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    backgroundColor: '#F7F7F7',
   };
-
+  const styles = StyleSheet.create({
+    container: {backgroundColor: backgroundStyle.backgroundColor, flex: 1},
+    text: {
+      fontSize: 25,
+      fontWeight: '500',
+    },
+  });
   return (
-    <SafeAreaView style={backgroundStyle}>
+    <SafeAreaView style={styles.container}>
       <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        barStyle={'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
-      <View></View>
+      <Button title="push" onPress={handleScheduleAlarm} />
     </SafeAreaView>
   );
 }
